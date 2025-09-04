624dd87a646e53e74c668defb8d4f2fc59abc1d4
Jonas Hertzman <jonas.hertzman@gmail.com>
2025-09-04 10:24:51 +0200

    Update download when publishing

---
+++ UpdateVersion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO.Compression;
+
+namespace UpdateVersion;
+
+class Program
+{
+	static void Main()
+	{
+		DateTime buildDate = DateTime.Now;
+		string buildNumber = $"{buildDate:yy}{buildDate.DayOfYear:D3}";
+
+		Console.WriteLine($"Updating version to {buildNumber}");
+
+		File.WriteAllText(@"..\docs\download\version.txt", buildNumber);
+
+
+		Console.WriteLine($"Updating download");
+
+		File.Delete(@"..\docs\download\DiskSize.zip");
+
+		using ZipArchive download = ZipFile.Open(@"..\docs\download\DiskSize.zip", ZipArchiveMode.Create);
+		download.CreateEntryFromFile(@".\bin\Publish\DiskSize.exe", "DiskSize.exe");
+		download.CreateEntryFromFile(@"..\LICENSE.txt", "LICENSE.txt");
+	}
+}
